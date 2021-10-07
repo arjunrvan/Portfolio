f3a190cbc07f863bfaf1a49511d0a1e286ee30f5
arjunrvan <arjunrvan@gmail.com>
2021-10-07 11:26:19 +0800

    added starting page

--- script.js
+++ script.js
@@ -59,10 +59,7 @@ function sendData () {
         msg: msgText.value
     })
 
-    nameText.value = "";
-    emailText.value = "";
-    subjectText.value = "";
-    msgText.value = "";
+    document.querySelector('form').reset();
 }
   
 // toggle menu / navbar script 
@@ -297,12 +294,12 @@ function eraseFun () {
     }
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    if (textArray.length) {setTimeout(typeWriter, newTextDelay)}; 
-});
+// window.addEventListener('DOMContentLoaded', function() {
+//     if (textArray.length) {setTimeout(typeWriter, newTextDelay)}; 
+// });
 
 
-// Set skills ani
+// Set skills animation
 
 var redlineH = document.getElementById('redlineH');
 var redlineC = document.getElementById('redlineC');
@@ -324,6 +321,98 @@ function setAni () {
         redlineP.classList.remove('redlineP');
         redlineS.classList.remove('redlineS');
     }, 3000);
+}
+
+
+// Function to enter the site
+var startingPage = document.querySelector('.starting-page');
+var container = document.getElementById('container');
+var enterBtn = document.querySelector('button');
+var progressCont = document.getElementById('progress-cont');
+var progressBar = document.getElementById('progress-bar'); 
+var progressText = document.getElementById('progress-text');
+var widthBar = 0;
+
+function enterSite () {
+    enterBtn.disabled = 'true';
+    enterBtn.style.cursor = 'auto';
+    enterBtn.style.opacity= '0';
+    progressText.style.opacity = '1';
+    progressCont.style.opacity = '1';
+
+    var barInt = setInterval(function () {
+        widthBar ++;
+        progressBar.style.width = widthBar + '%';
+        if (widthBar >= 100) {
+            clearInterval(barInt);
+            startingPage.classList.add('fadeOut');
+            startingPage.style.display = 'none';
+            container.style.display = 'block';
+            if (textArray.length) {setTimeout(typeWriter, newTextDelay)};
+        }
+    },50);
+
+}
+
+// Mouse move function
+
+var flyJet = document.getElementById('fly');
+
+document.addEventListener("mousemove", getMouse);
+
+var flypos = {x:0, y:0};
+
+setInterval(followMouse, 50);
+
+var mouse = {x:0, y:0}; //mouse.x, mouse.y
+
+var dirX = "right";
+var dirY = "down";
+
+function getMouse(e){
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+
+//Checking directional change
+    if(mouse.x > flypos.x){
+        dirX = "right";
+    } else {
+        dirX = "left";
+    }
+
+    if(mouse.y > flypos.y){
+        dirY = "down";
+    } else {
+        dirY = "up";
+    }
+}
+
+function followMouse(){
+    //1. find distance X , distance Y
+    var distX = mouse.x - flypos.x;
+    var distY = mouse.y - flypos.y;
+    //Easing motion
+    //Progressive reduction of distance 
+    flypos.x += distX/5;
+    flypos.y += distY/2;
+    
+    flyJet.style.left = flypos.x + "px";
+    flyJet.style.top = flypos.y + "px";
+
+
+//Apply css class 
+    if (dirX == "right"){
+    flyJet.setAttribute("class", "fly-right");
+    } else {
+    flyJet.setAttribute("class", "fly-left");        
+    }
+
+    if (dirY == "down"){
+        flyJet.setAttribute("class", "fly-down");
+        } else {
+        flyJet.setAttribute("class", "fly-up");        
+        }     
+    
 }
 
 
